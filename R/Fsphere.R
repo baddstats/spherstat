071e37649f9813e0a260c871a0fe88f4126fa1a9
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2015-10-27 18:05:00 +0800

    Reformatted code using standard indentation

--- R/Fsphere.R
+++ R/Fsphere.R
@@ -1,26 +1,40 @@
-Fsphere <-
-function(X, refpoints, win=sphwin(type="sphere"), rvals=seq(from=0, to=pi, length=512), ...) {
-stopifnot(inherits(X, c("sp2", "sp3", "matrix")) && inherits(refpoints, c("sp2", "sp3", "matrix")))
-if(inherits(X, "matrix")) {stopifnot(inherits(win, "sphwin"))} else {
-win <- X$win
-X <- X$X
-stopifnot(inherits(win, "sphwin"))
-}
-rad <- win$rad
-if(inherits(refpoints, c("sp2", "sp3")) && refpoints$win$rad != rad) {stop("X and refpoints have different radii")}
-if(inherits(refpoints, "matrix")) {refpoints <- refpoints} else {
-refpoints <- refpoints$X}
-D <- nncrosssph(X=X, Y=refpoints)
-B <- bdist.sphwin(X=refpoints, win=win)
-lambda <- intensitysph(X=X, win=win)
-r <- rad*rvals
-h <- eroded.areas.sphwin(win=win, r=r, ...)
-if(any(!is.finite(h))) {stop("Error: h not finite")}
-f <- compileCDF(D=D,B=B, r=r, han.denom=h, check=FALSE)
-dn <- fvnames(f, ".")
-f <- rebadge.fv(f, new.fname="Fsphere")
-ftheo <- 1 - exp(-lambda *2 * pi * (rad^2) * (1-cos(r/rad)))
-f <- bind.fv(f, data.frame(theo=ftheo), labl="%s[theo](r)", desc="theoretical value of %s")
-fvnames(f, ".") <- c(dn, "theo")
-return(f)
+Fsphere <- function(X, refpoints,
+                    win=sphwin(type="sphere"),
+                    rvals=seq(from=0, to=pi, length=512), ...) {
+  stopifnot(inherits(X, c("sp2", "sp3", "matrix")) &&
+            inherits(refpoints, c("sp2", "sp3", "matrix")))
+  if(inherits(X, "matrix")) {
+    stopifnot(inherits(win, "sphwin"))
+  } else {
+    win <- X$win
+    X <- X$X
+    stopifnot(inherits(win, "sphwin"))
+  }
+  rad <- win$rad
+  if(inherits(refpoints, c("sp2", "sp3")) &&
+     refpoints$win$rad != rad) {
+    stop("X and refpoints have different radii")
+  }
+  if(inherits(refpoints, "matrix")) {
+    refpoints <- refpoints
+  } else {
+    refpoints <- refpoints$X
+  }
+  D <- nncrosssph(X=X, Y=refpoints)
+  B <- bdist.sphwin(X=refpoints, win=win)
+  lambda <- intensitysph(X=X, win=win)
+  r <- rad*rvals
+  h <- eroded.areas.sphwin(win=win, r=r, ...)
+  if(any(!is.finite(h))) {
+    stop("Error: h not finite")
+  }
+  f <- compileCDF(D=D,B=B, r=r, han.denom=h, check=FALSE)
+  dn <- fvnames(f, ".")
+  f <- rebadge.fv(f, new.fname="Fsphere")
+  ftheo <- 1 - exp(-lambda *2 * pi * (rad^2) * (1-cos(r/rad)))
+  f <- bind.fv(f, data.frame(theo=ftheo),
+               labl="%s[theo](r)",
+               desc="theoretical value of %s")
+  fvnames(f, ".") <- c(dn, "theo")
+  return(f)
 }
