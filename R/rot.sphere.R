60d9a3e8c0a800c2bb5126d9ccef12e0980f0471
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2015-10-27 12:30:32 +0800

    More bug fixes

--- R/rot.sphere.R
+++ R/rot.sphere.R
@@ -1,11 +1,21 @@
-rot.sphere <-
-function(points, northpole, rad=1, inverse=FALSE) {
-stopifnot(inherits(points, c("sp2", "sp3", "matrix")))
-if(inherits(points, c("sp2", "sp3"))) {points1 <- points$X
-rad <- points$win$rad} else {points1 <- points}
-if(ncol(points1)==2){points1 <- convert3(points1)}
-rotmat <- rot.matrix(northpole)
-if(inverse) {rotpoints <- t(rad*(t(rotmat) %*% t(points1)))} else {rotpoints <- t(rad*(rotmat %*% t(points1)))}
-output <- convert2(sround(rotpoints))
-output
+rot.sphere <- function(points, northpole, rad=1, inverse=FALSE) {
+  if(inherits(points, c("sp2", "sp3"))) {
+    points1 <- points$X
+    rad <- points$win$rad
+  } else if(is.matrix(points)) {
+    points1 <- points
+  } else if(is.numeric(points) && length(points) %in% c(2,3)) {
+    points1 <- matrix(points, nrow=1)
+  } else stop("points should be a point pattern or a matrix")
+  if(ncol(points1)==2){
+    points1 <- convert3(points1)
+  }
+  rotmat <- rot.matrix(northpole)
+  if(inverse) {
+    rotpoints <- t(rad*(t(rotmat) %*% t(points1)))
+  } else {
+    rotpoints <- t(rad*(rotmat %*% t(points1)))
+  }
+  output <- convert2(sround(rotpoints))
+  output
 }
