a7609078b5c622fe958b52c2c1317a7e4331b78f
tjlawrence0 <tjlawrence@bigpond.com>
2015-10-29 10:26:44 +0800

    Test change to make rot.matrix faster
    test

--- R/rot.matrix.R
+++ R/rot.matrix.R
@@ -1,12 +1,16 @@
 rot.matrix <-
 function(northpole, rad=1) {
-if(class(northpole) != "matrix") {northpole1 <- t(matrix(northpole))} else {northpole1 <- northpole}
+if(!is.matrix(northpole)) {northpole <- t(matrix(northpole))}
 A <- matrix(nrow=3, ncol=3)
-A[3,] <- convert3(northpole1)/rad
-theta <- northpole1[1,1]
-phi <- northpole1[1,2]
-A[1,] <- c(cos(theta)*cos(phi), cos(theta)*sin(phi), -sin(theta))
-A[2,] <- c(-sin(phi), cos(phi), 0)
-for(i in 1:3){for(j in 1:3) {if(-(10^(-16)) <= A[i,j] && (10^(-16)) >= A[i,j]) {A[i,j] <- 0}}}
+theta <- northpole[1,1]
+phi <- northpole[1,2]
+ctheta <- cos(theta)
+stheta <- sin(theta)
+cphi <- cos(phi)
+sphi <- sin(phi)
+A[1,] <- c(ctheta*cphi, ctheta*sphi, -stheta)
+A[2,] <- c(-sphi, cphi, 0)
+A[3,] <- c(stheta*cphi, stheta*sphi, ctheta)
+A <- sround(cround(A))
 A
 }
