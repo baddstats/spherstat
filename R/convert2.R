9213c3caa99c5c0b790309b77d8e1d89b02e32ca
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2015-10-31 18:11:26 +0800

    Corrected and accelerated convert2, convert3

--- R/convert2.R
+++ R/convert2.R
@@ -1,22 +1,16 @@
-convert2 <- 
-function(points, rad=1) {
-  # ajb: in the next line, 'X' has not yet been defined
-stopifnot(length(points) ==3 || ncol(points) == 3 || inherits(X, "sp3"))
-X <- points
-if(inherits(X, "sp3")) {
-rad <- points$win$rad
-points <- points$X
-}
-if(!inherits(points, "matrix")) {points1 <- matrix(points, ncol=3)/rad} else {points1 <- points/rad}
-# ajb: 'n' is defined but not used 
-n <- nrow(points1)
-# ajb: is it correct to divide by 'rad' here again?
-# ajb: The expression points1[,3]/rad is re-evaluated several times.
-theta <- ifelse(1-(points1[,3]/rad) <= 0,  0, ifelse(1+(points1[,3]/rad) <= 0, pi, acos(cround(points1[,3]/rad))))
-stheta <- sin(theta)
-# ajb: 10^-15 is re-evaluated each time. Use 1e-15 instead.
-phi <- ifelse(abs(stheta)<10^-15, 0, atan2((points1[,2]/stheta), (points1[,1]/stheta))%%(2*pi))
-output <- cbind(theta, phi)
-if(inherits(X, "sp3")) {output <- sp2(X=output, win=X$win)}
-output
+convert2 <- function(points, rad=1) {
+  stopifnot(length(points) ==3 || ncol(points) == 3 || inherits(points, "sp3"))
+  X <- points
+  if(inherits(X, "sp3")) {
+    rad <- points$win$rad
+    points <- points$X
+  }
+  if(!is.matrix(points)) {
+    points <- matrix(points, ncol=3)/rad
+  }
+  theta <- acos(cround(points[,3]/rad))
+  phi <- atan2(points[,2], points[,1]) %% (2*pi)
+  output <- cbind(theta, phi)
+  if(inherits(X, "sp3")) {output <- sp2(X=output, win=X$win)}
+  output
 }
--- R/convert3.R
+++ R/convert3.R
@@ -1,21 +1,20 @@
-convert3 <-
-function(points, rad=1) {
-stopifnot(length(points) == 2 || ncol(points)==2 || inherits(points, "sp2"))
-points1 <- points
-# ajb: 'points1' is defined but never used
-if(inherits(points, "sp2")) {
-rad <- points$win$rad
-points <- points$X
-}
-# ajb: next line coul generate an error: class(points) could be a vector
-if(class(points) != "matrix") {points <- matrix(points, ncol=2, byrow=TRUE)} else {points <- points}
-stopifnot(ncol(points)==2)
-# ajb: 'n' is defined but not used
-n <- nrow(points)
-# ajb: sin(points[,1]) is re-evaluated several times
-points3 <-  cbind(sin(points[,1]) * cos(points[,2]),
-                       sin(points[,1]) * sin(points[,2]),
-                       cos(points[,1]))
-points3 <- sround(rad * points3)
-points3
+convert3 <- function(points, rad=1) {
+  stopifnot(length(points) == 2 || ncol(points)==2 || inherits(points, "sp2"))
+  if(inherits(points, "sp2")) {
+    rad <- points$win$rad
+    points <- points$X
+  }
+  if(!is.matrix(points)) {
+    points <- matrix(points, ncol=2, byrow=TRUE)
+  } 
+  stopifnot(ncol(points)==2)
+  theta <- points[,1]
+  phi <- points[,2]
+  sintheta <- sin(theta)
+  points3 <-  cbind(sintheta * cos(phi),
+                    sintheta * sin(phi),
+                    cos(theta))
+  if(rad != 1) 
+    points3 <- sround(rad * points3)
+  return(points3)
 }
