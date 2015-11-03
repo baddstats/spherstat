92a565f5156f0e4fa5620615ed6db244004e8856
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2015-11-03 17:31:02 +0800

    Corrected mindist.polygon; updated version to 1.3-4

--- R/mindist.polygon.R
+++ R/mindist.polygon.R
@@ -1,21 +1,36 @@
-mindist.polygon <-
-function(X, win) {
-stopifnot(inherits(X, c("sp2", "sp3", "matrix")))
-if(inherits(X, "matrix")) {stopifnot(inherits(win, "sphwin"))} else {
-win <- X$win
-X <- X$X
+mindist.polygon <- function(X, win) {
+  stopifnot(inherits(X, c("sp2", "sp3", "matrix")))
+  if(inherits(X, "matrix")) {
+    stopifnot(inherits(win, "sphwin"))
+  } else {
+    win <- X$win
+    X <- X$X
+  }
+  rad <- win$rad
+  nX <- nrow(X)
+  if(nX == 0) return(Inf)
+  if(ncol(X) !=3) {X <- convert3(X)}
+  verts <- convert3(win$param)
+  nvert <- nrow(verts)  ## note: last vertex = first vertex
+  ## distance from each point to each vertex
+  dXV <- gcdist(x=X, y=verts, rad=rad)
+  dXVj      <- t(dXV[,-nvert,drop=FALSE])
+  dXVjplus1 <- t(dXV[,-1,    drop=FALSE])
+  ## polygon edge lengths
+  elen <- gcdistPaired(x=verts[-nvert,,drop=FALSE],
+                       y=verts[-1,,drop=FALSE],
+                       rad=rad)
+  ## angle X[i], V[j], V[j+1]
+  ang1 <- sphcos(d1=dXVj, d2=elen, d3=dXVjplus1, theta=NULL, rad=rad)
+  ## angle X[i], V[j+1], V[j]
+  ang2 <- sphcos(d1=dXVjplus1, d2=elen, d3=dXVj, theta=NULL, rad=rad)
+  ## perpendicular distance from X[i] to great circle containing edge
+  perps <- sphsin(d1=dXVj, d2=NULL, theta1=pi/2, theta2=ang1, rad=rad)
+  ## distance from X[i] to edge V[j], V[j+1]
+  ##     If both angles are acute, use the perpendicular distance,
+  ##     otherwise the smaller of the point-to-vertex distances
+  dists <- ifelse(ang1 < pi/2 & ang2 < pi/2, perps, pmin(dXVj, dXVjplus1))
+  md <- if(!is.matrix(dists)) min(dists) else apply(dists, 2, min)
+  return(md)
 }
-rad <- win$rad
-n <- nrow(X)
-lp <- nrow(win$param)
-p3 <- convert3(win$param)
-if(ncol(X) !=3) {X <- convert3(X)}
-gc.pv1 <- gcdist(x=X, y=p3[1:lp,], rad=rad)
-gc.pv2 <- t(gc.pv1[,1:(lp-1)])
-gc.pv3 <- t(gc.pv1[,2:lp])
-gc.v <- gcdistPaired(x=p3[1:(lp-1),], y=p3[2:lp,], rad=rad)
-angs <- sphcos(d1=gc.pv2, d2=gc.v, d3=gc.pv3, theta=NULL, rad=rad)
-dists <- sphsin(d1=gc.pv2, d2=NULL, theta1=pi/2, theta2=angs, rad=rad)
-if(n==1) {md <- min(dists)} else {md <- apply(dists, 2, min)}
-md
-}
+
