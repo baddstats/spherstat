478f595c1cd5fcd022f5974fa3abde155e16897f
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2016-01-01 12:27:30 +0800

    Corrected algorithms for rMaternI.sphwin, rMaternII.sphwin

--- R/rMaternI.sphwin.R
+++ R/rMaternI.sphwin.R
@@ -3,46 +3,41 @@ rMaternI.sphwin <- function(kappa, r, win=sphwin(type="sphere"), nsim=1, drop=TR
 	stopifnot(kappa > 0)
 	stopifnot(sum(ndim==c("2", "3")) == 1)
 	output <- list()
-	if(stationary==TRUE) {
-		wina <- sphwin(type="sphere", rad=win$rad)
-	}
-	else {
-		wina <- win
+	if(stationary) {
+          wina <- sphwin(type="sphere", rad=win$rad)
+	} else {
+          wina <- win
 	}
 	for(i in 1:nsim) {
-		X1 <- rpoispp.sphwin(lambda=kappa, win=wina, as.sp=FALSE)
-		nrX <- nrow(X1)
-		X2 <- runif.sphwin(n=1, win=wina, as.sp=FALSE)
-		for(j in 1:(nrX-1)) {
-			Xtest <- runif.sphwin(n=1, win=wina, as.sp=FALSE)
-			if(min(nncrosssph(Xtest, X2)) > r) {
-				X2 <- rbind(X2, Xtest)
-			}
-		}
-		if(stationary==TRUE) {
-			Xin <- in.W(X2, win)
-			X2 <- X2[Xin, , drop=FALSE]
-		}
-		output[[i]] <- switch(ndim,
-			"2" = {
-				X2
-			},
-			"3" = {
-				convert3(X2)
-			},
-			stop("ndim unrecognised.  See help file for details.")
-			)
-		if(as.sp) {
-			if(ndim=="2") {
-				output[[i]] <- sp2(output[[i]], win)
-			}
-			else {
-				output[[i]] <- sp3(output[[i]], win)
-			}
-		}
+          X1 <- rpoispp.sphwin(lambda=kappa, win=wina, as.sp=FALSE)
+          nnd <- nndistsph(X1)
+          retain <- (nnd >= r)
+          X2 <- X1[retain, , drop=FALSE]
+          if(stationary) {
+            Xin <- in.W(X2, win)
+            X2 <- X2[Xin, , drop=FALSE]
+          }
+          output[[i]] <-
+            switch(ndim,
+                   "2" = {
+                     X2
+                   },
+                   "3" = {
+                     convert3(X2)
+                   },
+                   stop("ndim unrecognised.  See help file for details.")
+                   )
+          if(as.sp) {
+            if(ndim=="2") {
+              output[[i]] <- sp2(output[[i]], win)
+            }
+            else {
+              output[[i]] <- sp3(output[[i]], win)
+            }
+          }
 	}
-	if(nsim==1 && drop==TRUE) {
-		output <- output[[1]]
+	if(nsim==1 && drop) {
+          output <- output[[1]]
 	}
 	output 
 }
--- R/rMaternII.sphwin.R
+++ R/rMaternII.sphwin.R
@@ -3,63 +3,47 @@ rMaternII.sphwin <- function(kappa, r, win=sphwin(type="sphere"), nsim=1, drop=T
 	stopifnot(inherits(win, "sphwin"))
 	stopifnot(kappa > 0)
 	stopifnot(r > 0)
-	if(stationary) {
-		wina <- sphwin(type="sphere")
-	}
-	else {
-		wina <- win
-	}
+	wina <- if(stationary) sphwin(type="sphere") else win
 	output <- list()
 	for(i in 1:nsim) {
-		X1 <- rpoispp.sphwin(lambda=kappa, win=wina, as.sp=FALSE)
-		nrX <- nrow(X1)
-		X2 <- runif.sphwin(n=1, win=wina, as.sp=FALSE)
-		for(k in 1:(nrX-1)) {
-			Xtest <- runif.sphwin(n=1, win=wina, as.sp=FALSE)
-			to.keep <- c()
-			nrX2 <- nrow(X2)
-			if(length(X2)==0) {
-				## cat("0 ")
-				X2 <- rbind(X2, Xtest)
-			}
-			else {
-				for(j in 1:nrX2) {
-					if(gcdist(Xtest, matrix(X2[j,], nrow=1, ncol=2, byrow=TRUE)) > r) {
-						to.keep <- c(to.keep, j)
-					}
-				}
-				if(length(to.keep) == nrX2) {
-					X2 <- rbind(X2, Xtest)
-				}
-				else {
-					X2 <- X2[to.keep,,drop=FALSE]
-				}
-			}
-		}
-		if(stationary) {
-			X2in <- in.W(X2, win)
-			X2 <- X2[X2in, , drop=FALSE]
-		}
-		output[[i]] <- switch(ndim,
-			"2" = {
-				X2
-			},
-			"3" = {
-				convert3(X2)
-			},
-			stop("ndim unrecognised.  See help file for details.")
-			)
-		if(as.sp) {
-			if(ndim=="2") {
-				output[[i]] <- sp2(output[[i]], win)
-			}
-			else {
-				output[[i]] <- sp3(output[[i]], win)
-			}
-		}
+          X <- rpoispp.sphwin(lambda=kappa, win=wina, as.sp=FALSE)
+          ## find all pairs of r-close points
+          close <- (pairdistsph(X) < r)
+          diag(close) <- FALSE
+          ## attach arrival times to points.
+          nrX <- nrow(X)
+          tX <- runif(nrX)
+          ## make a matrix which is TRUE if j arrived earlier than i
+          earlier <- outer(tX, tX, ">")
+          ## point i is killed if there is any earlier point j that is r-close
+          kill <- apply(close & earlier, 1, any)
+          ## delete killed points
+          X <- X[!kill, , drop=FALSE]
+          if(stationary) {
+            Xin <- in.W(X, win)
+            X <- X[Xin, , drop=FALSE]
+          }
+          output[[i]] <-
+            switch(ndim,
+                   "2" = {
+                     X
+                   },
+                   "3" = {
+                     convert3(X)
+                   },
+                   stop("ndim unrecognised.  See help file for details.")
+                   )
+          if(as.sp) {
+            if(ndim=="2") {
+              output[[i]] <- sp2(output[[i]], win)
+            }
+            else {
+              output[[i]] <- sp3(output[[i]], win)
+            }
+          }
 	}
-	if(nsim==1 && drop==TRUE) {
-		output <- output[[1]]
+	if(nsim==1 && drop) {
+          output <- output[[1]]
 	}
 	output
 }
