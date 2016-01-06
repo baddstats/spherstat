aca6593c2372b1d8f8d6aa66458562854e87d14d
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2016-01-06 09:48:50 +0800

    Implemented basic plot method

---
+++ R/plotting.R
@@ -0,0 +1,67 @@
+plot.sp3 <- plot.sp2 <- local({
+
+  is.globe.point <- function(x) {
+    is.list(x) && identical(names(x), c("lon", "lat"))
+  }
+
+  Convert.globe <- function(x) {
+    if(!is.matrix(x)) x <- matrix(x, nrow=1)
+    convert.globe(x)
+  }
+  
+  plot.sp2 <- function(x, ..., eye, top, add=FALSE) {
+    X <- convert.globe(x)
+    if(!add) {
+      globeearth(NULL)
+      plot(x$win, ..., add=TRUE)
+    }
+    if(missing(eye) || is.null(eye)) {
+      eye <- place('nedlands')
+    } else if(!is.globe.point(eye)) {
+      eye <- Convert.globe(eye)
+    }
+    if(missing(top) || is.null(top)) {
+      top <- place('northpole')
+    } else if(!is.globe.point(top)) {
+      top <- Convert.globe(top)
+    }
+    globepoints(X, eye=eye, top=top, ...)
+  }
+
+  plot.sp2
+})
+
+plot.sphwin <- function(x, ..., eye, top, add=FALSE) {
+  if(!add)
+    globeearth(NULL)
+  type  <- x$type
+  param <- x$param
+  ref   <- x$ref
+  curve1 <- curve2 <- NULL
+  switch(type,
+         sphere = { },
+         band = ,
+         bandcomp = {
+           fullcircle <- seq(0, 2*pi, length=1000)
+           if(param[1] != 0)
+             curve1 <- rot.sphere(cbind(param[1], fullcircle),
+                                  northpole=ref)
+           if(param[2] != pi)
+             curve2 <- rot.sphere(cbind(param[2], fullcircle),
+                                  northpole=ref)
+         },
+         wedge = ,
+         polygon = ,
+         quadrangle = {
+           warning(paste("Plotting is not yet implemented for windows of type",
+                         sQuote(type)))
+         })
+  if(!is.null(curve1))
+    globelines(convert.globe(curve1), ..., eye=eye, top=top)
+  if(!is.null(curve2))
+    globelines(convert.globe(curve2), ..., eye=eye, top=top)
+  return(invisible(NULL))
+}
+
+
+
