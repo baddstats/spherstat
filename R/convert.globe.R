713d620b7b09821eea07ba27fa418fe4d4970583
tjlawrence0 <tjlawrence@bigpond.com>
2016-02-14 16:44:00 +0800

    Enabled coordinate converters to convert both ways
    convert.globe and convert.sphereplot are now able to convert coordinates
    from those that the globe and sphereplot packages require, to those that
    spherstat requires

--- R/convert.globe.R
+++ R/convert.globe.R
@@ -1,14 +1,34 @@
-convert.globe <- function(X) {
-  stopifnot(inherits(X, c("sp2", "sp3", "matrix")))
-  if(!inherits(X, "matrix")) 
-    X <- X$X
-  if(ncol(X)==3) {
-    X <- convert2(X)
-  }
-  Xdegrees <- X*(180/pi)
-  Xlat <- 90 - Xdegrees[,1]
-  Xlon <- Xdegrees[,2]
-  Xlon <- ifelse(Xlon <= 180, Xlon, Xlon - 360)
-  Xglobe <- cbind(lon=Xlon, lat=Xlat)
-  return(Xglobe)
+convert.globe <- function(X, inverse=FALSE, sp.dim=2) {
+	if(inverse) {
+		stopifnot(sp.dim==2 || sp.dim==3)
+		stopifnot(inherits(X, c("list", "matrix"))  & !inherits(X, c("sp2", "sp3")))
+		if(inherits(X, "list")) {
+			Xlon <- X[[1]]
+			Xlat <- X[[2]]
+		}
+		else {
+			Xlon <- X[,1]
+			Xlat <- X[,2]
+		}
+		Xlon <- ifelse(Xlon >= 0, Xlon, Xlon+360)
+		X <- cbind(90-Xlat, Xlon)*(pi/180)
+		if(sp.dim==3) {
+			X <- convert3(X)
+		}
+	}
+	else {
+		stopifnot(inherits(X, c("sp2", "sp3", "matrix")))
+		if(!inherits(X, "matrix")) {
+			X <- X$X
+		}
+		if(ncol(X)==3) {
+			X <- convert2(X)
+		}
+		Xdegrees <- X*(180/pi)
+		Xlat <- 90 - Xdegrees[,1]
+		Xlon <- Xdegrees[,2]
+		Xlon <- ifelse(Xlon <= 180, Xlon, Xlon - 360)
+		X <- list(lon=Xlon, lat=Xlat)
+	}
+	return(X)
 }
--- R/convert.sphereplot.R
+++ R/convert.sphereplot.R
@@ -1,15 +1,26 @@
-convert.sphereplot <- function(X) {
-	stopifnot(inherits(X, c("sp2", "sp3", "matrix")))
-	if(inherits(X, "matrix")) {
-		X <- X
+convert.sphereplot <- function(X, inverse=FALSE, sp.dim=2) {
+	if(inverse) {
+		stopifnot(sp.dim==2 || sp.dim==3)
+		stopifnot(inherits(X, c("matrix", "numeric")))
+		X <- cbind(90-X[,1], X[,2])
+		X <- X*(180/pi)
+		if(sp.dim==3) {
+			X <- convert3(X)
+		}
 	}
 	else {
-	X <- X$X
+		stopifnot(inherits(X, c("sp2", "sp3", "matrix")))
+		if(inherits(X, "matrix")) {
+			X <- X
+		}
+		else {
+			X <- X$X
+		}
+		if(ncol(X)==3) {
+			X <- convert2(X)
+		}
+		X <- X*(180/pi)
+		X <- cbind(X[,2], (90-X[,1]))
 	}
-	if(ncol(X)==3) {
-		X <- convert2(X)
-	}
-	Xsp <- X*(180/pi)
-	Xsp.out <- cbind(Xsp[,2], (90-Xsp[,1]))
-	Xsp.out
+	X
 }
