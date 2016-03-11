f7aea43a639e506a57e8b050f6f02f2d495405b0
tjlawrence0 <tjlawrence@bigpond.com>
2016-03-11 20:44:51 +0800

    Enabled cround and sround to handle lists, dataframes
    Transform of summary function code required cround, sround to be used on
    lists or dataframes, but those functions were designed to work on
    numerics, vectors and matrices.  They can now work on lists and data
    frames.

--- R/cround.R
+++ R/cround.R
@@ -1,3 +1,10 @@
 cround <- function(x) {
-	ifelse(x < -1, -1, ifelse(x > 1, 1, x))
+	if(inherits(x, "list") || inherits(x, "data.frame")) {
+		for(i in 1:length(x)) {
+			ifelse(x[[i]] < -1, -1, ifelse(x[[i]] > 1, 1, x))
+		}
+	}
+	else {
+		ifelse(x < -1, -1, ifelse(x > 1, 1, x))
+	}
 }
--- R/sround.R
+++ R/sround.R
@@ -1,4 +1,11 @@
 sround <- function(x) {
-	ifelse(abs(x) < 1e-7 , 0, x)
+	if(inherits(x, "list") || inherits(x, "data.frame")) {
+		for(i in 1:length(x)) {
+			ifelse(abs(x[[i]]) < 1e-7, 0, x[[i]])
+		}
+	}
+	else {
+			ifelse(abs(x) < 1e-7 , 0, x)
+	}
 }
 
