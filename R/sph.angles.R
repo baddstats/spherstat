c5ced84b483dd91195cd09e8b135bac804fbd799
tjlawrence0 <tjlawrence@bigpond.com>
2020-03-15 17:03:49 +1100

    Corrections to algorithms so that they work for some polygons
    In particular, polygons with vertices v_1, ... v_n,  where for at least
    one value of i between 1 and n, the distance between vertices v_i and
    v_i+2 (or v_i-2) through the polygon is greater than pi*rad.

--- R/sph.angles.R
+++ R/sph.angles.R
@@ -6,15 +6,52 @@ sph.angles <- function (win) {
 	else {
 		win <- win$win
 	}
+	angles <- switch(win$type,
+		wedge = {
+			rep(win$param[1], 2)
+		},
+		quadrangle = {
+			rep(pi/2, 4)
+		},
+		polygon = {
+			sph.angles.poly(win=win)
+		},
+		{
+		"This shape does not have any angles."
+		}	
+	)
+	angles
+}
+
+## sph.angles.poly gives the algorithm for calculating the size of the angles in a polygon
+## The algorithm being used here is split the angle at each vertex into two, by drawing a great circle arc from ref3 to the vertex that is entirely within the polygon.
+## We can calculate the size of the two angles created to get the size of the angle at that vertex of the polygon (the sphcos commands at the end of the code; the preceding lines of code are used to get the information that the sphcos commands require)
+## The algorithm is designed to simultaneously perform this calculation for all polygons.
+
+sph.angles.poly <- function(win) {
+	stopifnot(inherits(win, c("sp2", "sp3", "sphwin")))
+	if(inherits(win, "sphwin")) {
+		win <- win
+	}
+	else {
+		win <- win$win
+	}
+	stopifnot(win$type=="polygon")
 	rad <- win$rad
-	wp3 <- convert3(win$param)
-	lp <- nrow(wp3)
-	vert1 <- wp3[1:(lp-1),]
-	vert2 <- wp3[2:lp,]
-	vert3 <- rbind(wp3[3:lp,], wp3[2,])
-	dist1 <- gcdistPaired(x=vert1, y=vert2, rad=rad)
-	dist2 <- gcdistPaired(x=vert2, y=vert3, rad=rad)
-	dist3 <- gcdistPaired(x=vert1, y=vert3, rad=rad)
-	angles <- mapply(sphcos, d1=dist1, d2=dist2, d3=dist3, MoreArgs=list(theta=NULL, rad=1))
+	param3 <- convert3(win$param)
+	lp <- nrow(param3)
+	ref3 <- ref3
+	polyangs1 <- param3[1:(lp-1),]
+	polyangs2 <- param3[2:lp,]
+	polyangs3 <- rbind(param3[3:lp, ], param3[2,])
+	gc <- gcdistPaired(x=polyangs1, y=polyangs2, rad=rad)
+	gc1 <- gc[1:(lp-1)]
+	gc2 <- c(gc[2:(lp-1)], gc[1])
+	gc.r3v1 <- gcdist(x=ref3, y=polyangs1)
+	gc.r3v2 <- gcdist(x=ref3, y=polyangs2)
+	gc.r3v3 <- gcdist(x=ref3, y=polyangs3)
+	sc1 <- sphcos(d1=gc1, d2=gc.pv2, d3=gc.pv1, theta=NULL, rad=rad)
+	sc2 <- sphcos(d1=gc2, d2=gc.pv2, d3=gc.pv3, theta=NULL, rad=rad)
+	angles <- sc1+sc2	
 	angles
 }
